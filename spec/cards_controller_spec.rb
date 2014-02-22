261b0b3daad8a823de9e5ca3b057eb8ad0b32d86
Stephen Buchanan & Quentin Devauchelle <pair+papapoison.qt-dev@devbootcamp.com>
2014-02-21 19:18:29 -0800

    Adds the 2 cards routes, with tests for the usecases

---
+++ spec/cards_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'cool_faker'
+
+describe 'cards controller' do
+  before do
+    @card = Card.create(question: "Have you ever danced with the devil in the pale moonlight?", answer: "Bruce, your parents are dead")
+    10.times { Card.create(question: CoolFaker::Team.slogan, answer: CoolFaker::Team.slogan) }
+  end
+
+  it "should redirect on get '/cards'" do
+    get '/cards'
+    expect(last_response.redirect?).to be_true
+  end
+
+  it "should redirect if the answer is right" do
+    get "/cards/#{@card.id}", answer: @card.answer
+    expect(last_response.redirect?).to be_true
+  end
+
+  after do
+    Card.all.each { |card| Card.find(card.id).destroy }
+  end
+
+end
