d671abfa4b544b042dbc486637174a043ff423f3
Apprentice <apprentice@dbc09.local>
2014-02-21 16:17:37 -0800

    Add right or wrong view add play view modified controller index and select deck index play doesnt work yet, need to save the state of the card

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,7 @@
+def right_answer?(card, answer)
+  return card.answer == answer
+end
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
@@ -12,8 +16,27 @@ get '/log_in' do
 end
 
 get '/select_deck' do
+
+  # @decks = Card.all
   erb :select_deck
 end
 
-get '/play/deck:id' do
+# get '/play/deck/id:id' do
+#   @cards = Card.all
+#   erb :play
+# end
+
+get '/play' do
+  @cards = Card.all
+  erb :play
+end
+
+get '/right_or_wrong' do
+#this doesn't work
+#becuase we can't keep the card that they're guessing;
+#we only have answer saved, not card
+
+  @card = something
+  @answer = params[:answer]
+  erb :right_or_wrong
 end
