9935cb44ddf8d5b72ae35e040a8b848e0ec50e5b
Quentin Devauchelle <quentin@devauchelle.eu>
2014-02-24 00:45:36 -0800

    Adds new questions haha

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 
-Deck.create(name: "Banana Slug Trivia", cards: [Card.create(question: "The french dude", answer: "Quentin"),
+Deck.create(name: "Banana Slug Trivia", cards: [
+Card.create(question: "The french dude", answer: "Quentin"),
 Card.create(question: "The wizard", answer: "Topher"),
 Card.create(question: "The tall guy", answer: "Tim"),
 Card.create(question: "Papa Poison", answer: "Stephen"),
@@ -11,7 +12,19 @@ Card.create(question: "*insert meow here*", answer: "Rao Rao"),
 Card.create(question: "Sweet awesomeness", answer: "Shadi"),
 Card.create(question: "Led Zepplin", answer: "Phil"),
 Card.create(question: "Apparently a god of pool", answer: "Sherif"),
-Card.create(question: "Thinks farting is hilarious", answer: "Armondo")])
+Card.create(question: "Thinks farting is hilarious", answer: "Armando"),
+Card.create(question: "Our home", answer: "Dev Bootcamp"),
+Card.create(question: "Cookie Monster", answer: "Quentin"),
+Card.create(question: "Most important post punk of all time", answer: "Joy Division"),
+Card.create(question: "The king of algorithms", answer: "Hunter"),
+Card.create(question: "The guy who runs on linux", answer: "Ivan"),
+Card.create(question: "El don", answer: "Jose"),
+Card.create(question: "Our biggest loss", answer: "Erin"),
+Card.create(question: "Fucking...", answer:"Steve"),
+Card.create(question: "Secretly loves country music", answer:"Jamie"),
+Card.create(question: "Gives the best Lightning talks ever", answer:"Jamie"),
+Card.create(question: "Meatballs...", answer:"Chris")
+])
 
 
 Deck.create(name: "Urban Dictionary Trivia", cards: [Card.create(question: "you only live once, might as well make it tasty", answer: "yolowmit"),
